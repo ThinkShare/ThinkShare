e7f0ab1c8f82870c3fe458f8614ff85d59f33f9c
epitsin <epitsin@yahoo.com>
2014-09-17 12:27:11 +0300

    added json formatter

--- ThinkShareCodeFirst/ThinkShare.Services/App_Start/WebApiConfig.cs
+++ ThinkShareCodeFirst/ThinkShare.Services/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using System.Net.Http.Headers;
     using System.Web.Http;
     using System.Web.OData.Extensions;
 
@@ -21,6 +22,8 @@
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
